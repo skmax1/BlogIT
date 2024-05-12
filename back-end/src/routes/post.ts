19049d470f2aa49abab2572c155ae709f96d3d92
skmax1 <keshav.shri06@gmail.com>
2024-05-12 14:54:28 +0530

    bug: fixed post routes

--- back-end/src/routes/post.ts
+++ back-end/src/routes/post.ts
@@ -1,12 +1,36 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono"
+import { verify } from "hono/jwt";
 
 export const postRouter = new Hono<{
     Bindings:{
-        DATABASE_URL: string,       
+        DATABASE_URL: string,
+        JWT_SECRET: string       
+    }
+    Variables:{
+        userId: string
     }
 }>();
+
+//middleware function
+
+postRouter.use('/*', async (c,next) =>{
+    const authHeader = c.req.header("Authorization") || "";
+    const decoded = await verify(authHeader,c.env.JWT_SECRET)
+    if(decoded){
+
+        c.set("userId",decoded.id)
+        await next();
+    }
+    else {
+        c.status(403);
+        c.json({
+            msg: "Auth failed"
+        })
+    }
+    
+})
 
 //creating new post
 postRouter.post('/', async (c) => {
@@ -15,13 +39,15 @@ postRouter.post('/', async (c) => {
         datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate())
 
+    const authourId = c.get("userId")
+
     const body = await c.req.json();
 
     const post = await prisma.post.create({
         data:{
             title: body.title,
             writeup: body.writeup,
-            authourId: '1'
+            authourId: authourId
         }
     })
 
@@ -56,11 +82,39 @@ postRouter.post('/', async (c) => {
     })
     
   })
+
+
+    //get all post
+  
+    postRouter.get('/bulk',async (c) => {
+        const prisma  = new PrismaClient({
+            datasourceUrl: c.env.DATABASE_URL
+        }).$extends(withAccelerate())
+    
+    
+        try{
+            const bulk = await prisma.post.findMany();
+    
+            return c.json({
+                msg: "Fetch sucessfull",
+                bulk
+            })
+    
+        }catch(e){
+            c.status(411);
+            console.log(e);
+            c.json({
+                msg: 'Error while fetching'
+            })
+            
+        }
+    
+      })
   
   //getting a post with id param
   postRouter.get('/:id', async (c) => {
 
-    const id = await c.req.param('id');
+    const id =  c.req.param('id');
 
     const prisma = new PrismaClient({
         datasourceUrl: c.env.DATABASE_URL
@@ -89,30 +143,5 @@ try{
     
   })
 
-  //get all post
-  
-  postRouter.get('/bulk',async (c) => {
-    const prisma  = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL
-    }).$extends(withAccelerate())
 
-
-    try{
-        const bulk = await prisma.post.findMany();
-
-        return c.json({
-            msg: "Fetch sucessfull",
-            bulk
-        })
-
-    }catch(e){
-        c.status(411);
-        console.log(e);
-        c.json({
-            msg: 'Error while fetching'
-        })
-        
-    }
-
-  })
   
